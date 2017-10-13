5290a90923acd44b243f931870eefb2623ef2227
Tony Tung <ttung@chanzuckerberg.com>
2017-10-13 13:43:41 -0700

    Encode the version in a file.  Read it to provide that data.
    Importing the module from setup.py is problematic because that implies the user has everything installed required to import the module.  This circumvents the issue.

--- showit/__init__.py
+++ showit/__init__.py
@@ -1,3 +1,24 @@
+import json
+import os
 from .showit import (tile, image)
 
-__version__ = '1.1.1'
+def get_version():
+    version_json = None
+    dirname = os.path.dirname(__file__)
+    for candidatepath in (
+            [dirname, ".."], # this is the non-installed path
+            [dirname, "..", "..", "..", "..", "share", "showit"],
+            ):
+        try:
+            candidatepath.append("VERSION")
+            with open(os.path.join(*candidatepath), "r") as fh:
+                version_json = json.loads(fh.read())
+        except (IOError, OSError):
+            pass
+
+    if version_json is None:
+        raise ValueError("Cannot find VERSION file")
+
+    return version_json['string']
+
+__version__ = get_version()
